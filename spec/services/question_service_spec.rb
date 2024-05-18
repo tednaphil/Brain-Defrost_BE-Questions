9d0df83a0443ec105542c0dd52a039ee9cb6d7a8
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-05-18 14:11:18 -0500

    question service
    Co-authored-by: Martin Chavez <140449078+Chavezgm@users.noreply.github.com>
    Co-authored-by: Laura R Vega <149700247+laurarvegav@users.noreply.github.com>

---
+++ spec/services/question_service_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe QuestionService do
+  before do
+    @question_number = 3
+    @topic = "music"
+  end
+ 
+  describe '.create_trivia_questions(topic, question_number)' do
+    context 'happy path' do
+      it 'returns generated trivia questions', :vcr do
+        service_response = QuestionService.create_trivia_questions(@topic, @question_number)
+
+        expect{ service_response }.not_to raise_error
+        check_hash_structure(service_response, :data, Array)
+        
+        service_response[:data].each do |data|
+          check_hash_structure(data, :id, String)
+          check_hash_structure(data, :attributes, Hash)
+          check_hash_structure(data[:attributes], :question_text, String)
+          check_hash_structure(data[:attributes], :correct_answer, String)
+          check_hash_structure(data[:attributes], :answer_options, Array)
+          expect(data[:attributes][:answer_options].size).to eq(4)
+          expect(data[:attributes][:answer_options]).to all(be_a(String))
+        end
+      end
+    end
+
+    context 'sad path' do
+      it "returns error message if request unsuccessful" do
+        stub_request(:post, 'https://api.openai.com/v1/chat/completions')
+          .to_return(status: 500)
+
+        service_response = QuestionService.create_trivia_questions(@topic, @question_number)
+        expect(service_response).to eq("Unable to create trivia questions at this time")
+      end
+    end
+  end
+end
